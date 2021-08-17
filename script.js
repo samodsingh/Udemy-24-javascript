d7c8d4b9a8024c08a4232ba816249fa8bb57741b
samod singh <samod0098@gmail.com>
2021-08-17 16:58:51 +0530

    Udemy-26

---
+++ script.js
@@ -0,0 +1,23 @@
+let day = prompt("");
+switch (day){
+    case "Monday":
+   console.log("Plan course streture");
+   console.log("Go to coding meetup") ;
+   break;
+   case " tuesday":
+       console.log("prepare theory videos");
+       break;
+       case "wednesday":
+       case "thursday" :
+           console.log("write code examles");
+           break;
+           case "friday":
+               console.log("Record videos");
+               break;
+               case "saturday":
+               case "sunday" :
+                   console.log("Enjoy the weekend :D");
+    break;
+    default:
+        console.log("Not a valid day");
+}
